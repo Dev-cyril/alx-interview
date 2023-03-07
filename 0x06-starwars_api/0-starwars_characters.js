d872f41bcaf5281fa9396df3527fb96d8ca47d05
Cyril <cyril.idu2@gmail.com>
2023-03-07 16:15:42 +0000

    fixes

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,18 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
-request.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-    if (res.statusCode === 200) {
-        for (let char of JSON.parse(body).characters){
-            request.get(char, function (err, res, body){
-                if (res.statusCode === 200){
-                    console.log(JSON.parse(body).name)
-                } else if (err) {
-                    console.log(err);
-                }
-            })
-        }
-        console.log(JSON.parse(body).title);
-    } else if (err) {
-        console.log(err);
+request.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2], async (err, res, body) => {
+    err && console.log(err);
+    for (let chr of JSON.parse(body).characters){
+        await new Promise ((resolve, reject) => {
+            request(chr, (err, res, body) => {
+		err && console.log(err);
+                console.log(JSON.parse(body).name)
+		resolve();
+             });
+        });
     }
 });
