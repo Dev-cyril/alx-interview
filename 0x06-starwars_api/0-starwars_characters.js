898f57fe9e91d5a71cbdf3e372653be1f7b65bee
Cyril <cyril.idu2@gmail.com>
2023-03-07 16:20:12 +0000

    semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
 request.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2], async (err, res, body) => {
-    err && console.log(err);
-    for (let chr of JSON.parse(body).characters){
-        await new Promise ((resolve, reject) => {
-            request(chr, (err, res, body) => {
-		err && console.log(err);
-                console.log(JSON.parse(body).name)
-		resolve();
-             });
-        });
-    }
+  err && console.log(err);
+  for (const chr of JSON.parse(body).characters) {
+    await new Promise((resolve, reject) => {
+      request(chr, (err, res, body) => {
+        err && console.log(err);
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
 });
